44bc4fd43b9cb7070877a288950b39c70e6e337f
michel <programmer.michel@gmail.com>
2025-01-20 08:50:04 -0300

    inicio do projeto

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000);
+
