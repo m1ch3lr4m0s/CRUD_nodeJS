9be5cac002dc8ad49e9bd94f19d9f41946d0e48e
michel <programmer.michel@gmail.com>
2025-01-21 20:31:59 -0300

    finalização do projeto

--- data/conn.js
+++ data/conn.js
@@ -1,8 +1,11 @@
 const sequelize = require("sequelize");
-const db = new sequelize("crud_nodejs", "root", "j4v4n14n0", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mariadb', // ou 'mariadb', dependendo do seu banco de dados
+    port: process.env.DB_PORT,
 });
 
  try {
